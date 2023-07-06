7d65e072466d39022d2fdcce798b724e403d94c8
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-07-06 19:26:13 +0300

    add allowed hosts

--- astrofooding/settings.py
+++ astrofooding/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
-
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,9 @@ SECRET_KEY = 'django-insecure-mz%v9ytaef8o6&po(^yr9po4oeo^_h2li5)@0e(lv#jr8m(blq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'astrofooding-to.up.railway.app'
+]
 
 # Application definition
 
